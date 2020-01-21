5c748ec61e3705bb5ff791403cddf4d7e0e11739
asteinh <armin.steinhauser@kuleuven.be>
2020-01-21 12:03:58 +0100

    debug CI

--- build_docs.sh
+++ build_docs.sh
@@ -2,11 +2,6 @@
 
 CONTAINER=asteinh/casadi_docs
 
-cd content/docs
-sudo rm -rf -- */
-git clone git@github.com:asteinh/casadi_docs.git source
-cd source
-
 container=$(docker run -itd -v ${PWD}:/src $CONTAINER)
 docker exec -it $container /bin/bash -c "pip3 install -r requirements.txt"
 docker exec -it $container /bin/bash -c "ln -s /usr/bin/python3 /usr/bin/python"
@@ -17,12 +12,8 @@ for remote in $(git for-each-ref --format='%(refname)' refs/remotes); do
     local=${remote#refs/remotes/origin/}
     git checkout $local && git pull
     docker exec -it $container /bin/bash -c "make html"
-    sudo chown -R $(id -u):$(id -g) build/
+    chown -R $(id -u):$(id -g) build/
     folder=$(git rev-parse --abbrev-ref HEAD | sed -e 's/\.//g')
     mv build/$folder ../$folder
   fi
 done
-
-cd ..
-sudo rm -rf source/
-cd ../..
